b40647ea4e25f3f4b07c43893f5ba2dc9f1bfe73
sofiyathefirst <69139655+sofiyathefirst@users.noreply.github.com>
2022-11-02 03:00:29 +0800

    Changes to be committed: 	modified:   lib/services/firebase.dart

--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -1,7 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:test_project/services/user_model.dart';
 
 Future userSetup(
     String name, String pnum, String email, String password) async {
